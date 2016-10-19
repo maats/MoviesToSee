6ea016334bcd88393459c708fea8e33d8e6c4b46
Mateusz Kuchta <matikuchta@gmail.com>
2016-10-19 14:17:41 +0200

    Added settings dialog controller

---
+++ src/matradev/SettingsDialogController.java
@@ -0,0 +1,21 @@
+package matradev;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Mateusz on 19.10.2016.
+ */
+public class SettingsDialogController implements Initializable {
+
+    @FXML private Label lblToolbar;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
